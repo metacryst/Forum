270217bc3cf00ccbe9121733e2ea389ea4c049a0
metacryst <samrussell99@gmail.com>
2021-03-30 20:21:55 -0500

    Initial Commit

---
+++ Shared/ForumApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForumApp.swift
+//  Shared
+//
+//  Created by Sam Russell on 3/30/21.
+//
+
+import SwiftUI
+
+@main
+struct ForumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
