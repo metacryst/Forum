15225214ca21eca303208bdc769550d41c00d933
metacryst <samrussell99@gmail.com>
2021-04-29 20:53:02 -0500

    Made apple button shell

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,14 +6,21 @@
 //
 
 import SwiftUI
+import AuthenticationServices
+
+
 
 struct ContentView: View {
+    
+
+    
+    
+    
     var body: some View {
         
         let bounds = UIScreen.main.bounds
         let width = bounds.size.width
         let height = bounds.size.height
-        
         
         
     ZStack {
@@ -26,24 +33,41 @@ struct ContentView: View {
             Button(action: {
                 print("Round Action")
                 }) {
-                Text("")
-                    .frame(width: 100, height: 100)
-                    .foregroundColor(Color.black)
-                    .background(Color.red)
-                    .clipShape(Circle())
+
+                Image("appleLogo")
+                    .resizable()
+                    .frame(width: 130, height: 130)
+                    .padding(-18)
+                    .padding(.top, 7)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    
+                    
             }
-            .position(x: width/2, y: height/2)
+            .position(x: width/2, y: (height/2)+60)
             .frame(maxWidth: .infinity)
+            
+
+
+
         }
             
-            
-            
-            
-            
-            
     }
     }
+    
+    
+    
+    private func showAppleLogin() {
+        let request = ASAuthorizationAppleIDProvider().createRequest()
+        
+        request.requestedScopes = [.fullName, .email]
+        
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+    }
+    
 }
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
